dbb1d9b028be4f109634a12fac099238dbc5d5db
Miten Gajjar <miten3377@gmail.com>
2020-06-30 01:21:31 +0530

    added more cmnds

--- custom/plugins/Mi10/Mi10.plugin.zsh
+++ custom/plugins/Mi10/Mi10.plugin.zsh
@@ -1,13 +1,31 @@
 # Add your own custom plugins in the custom/plugins directory. Plugins placed
 # here will override ones with the same name in the main plugins directory.
  
+# Flutter commands
 alias fl="flutter"
 alias flr="flutter run"
 alias fldoc="flutter doctor"
 alias flb="flutter build"
+alias flbw="flutter build web"
 alias flattach="flutter attach"
 alias flget="flutter packages get"
 alias flc="flutter clean"
 alias flcr="flutter create"
 alias flrp="flutter run --profile"
 alias flrr="flutter run --release"
+
+# VS Code commands
+alias vsc="code ." 
+alias vs="code"
+
+# To edit .zshrc
+alias zshrc="code ~/.zshrc"
+
+# To install pkgs
+alias sapt="sudo apt install"
+alias saptg="sudo apt-get install"
+
+# To clear terminal Screen
+alias cls="clear"
+
+alias cd..="cd -"
