80e6b8971c95375b5252bc32a5cb08af18fc02f0
Miten Gajjar <miten3377@gmail.com>
2020-06-30 00:44:29 +0530

    added my settings

---
+++ custom/plugins/MyFlutter/MyFlutter.plugin.zsh
@@ -0,0 +1,10 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+ 
+alias fl="flutter"
+alias flr="flutter run"
+alias fldoc="flutter doctor"
+alias flb="flutter build"
+alias flattach="flutter attach"
+alias flget="flutter packages get"
+alias flc="flutter clean"
